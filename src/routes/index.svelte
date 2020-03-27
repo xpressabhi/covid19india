f931add1a10bd7ce5f1a1c2a760fcf80d96d781a
abhishek <akmnitt@gmail.com>
2020-03-27 23:06:13 +0530

    bootstrap

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,44 +1,90 @@
 <script>
 	import { onMount } from "svelte";
-	let data = [];
-	let date;
+	const dt = new Date();
+	const start = `${dt.getFullYear()}-0${dt.getMonth() +
+	  1}-${dt.getDate()}T00:00:00Z`;
+	let data;
+
+	let totalConfirmed = 0;
+	let newConfirmed = 0;
+	const confirmedurl = `https://api.covid19api.com/live/country/india/status/confirmed/date/${start}`;
 	onMount(async () => {
-	  const res = await fetch(
-	    "https://api.covid19api.com/live/country/india/status/confirmed"
-	  );
+	  const res = await fetch(confirmedurl);
 	  const json = await res.json();
-	  console.log(json);
-	  data = json.sort((a, b) => {
-	    return new Date(b.Date) - new Date(a.Date);
-	  });
-	  console.log(data);
+	  data = json.sort((a, b) => new Date(b.Date) - new Date(a.Date));
+	  newConfirmed = Number(data[0].Cases) - Number(data[data.length - 1].Cases);
+	  totalConfirmed = data[0].Cases;
+	});
+	let totalDeaths = 0;
+	let newDeaths = 0;
+	const deathsurl = `https://api.covid19api.com/live/country/india/status/deaths/date/${start}`;
+	onMount(async () => {
+	  const res = await fetch(deathsurl);
+	  const json = await res.json();
+	  data = json.sort((a, b) => new Date(b.Date) - new Date(a.Date));
+	  newDeaths = Number(data[0].Cases) - Number(data[data.length - 1].Cases);
+	  totalDeaths = data[0].Cases;
+	});
+	let totalRecovered = 0;
+	let newRecovered = 0;
+	const recoveredurl = `https://api.covid19api.com/live/country/india/status/recovered/date/${start}`;
+	onMount(async () => {
+	  const res = await fetch(recoveredurl);
+	  const json = await res.json();
+	  data = json.sort((a, b) => new Date(b.Date) - new Date(a.Date));
+	  newRecovered = Number(data[0].Cases) - Number(data[data.length - 1].Cases);
+	  totalRecovered = data[0].Cases;
 	});
 </script>
 <style>
-  table,
-  th,
-  td {
-    border: 1px solid black;
+  /* Stat */
+  .stat {
+    display: inline-flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+
+  .stat h3,
+  .stat h4 {
+    margin: 0;
+    font-size: 4rem;
+    font-weight: 100;
+  }
+
+  .stat p {
+    margin: 0;
+    font-weight: 100;
+  }
+
+  .stat.flex-row p {
+    padding-left: 0.5rem;
   }
 </style>
 <svelte:head>
 	<title>COVID-19 India</title>
 </svelte:head>
 
-<h1>Indian Confirmed Cases</h1>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Date</th>
-			<th scope="col">Cases</th>
-    </tr>
-  </thead>
-	{#each data as d}
-  <tbody>
-    <tr>
-      <td>{d.Date}</td>
-      <td>{d.Cases}</td>
-    </tr>
-  </tbody>
-	{/each}
-</table>
+<div class="jumbotron">
+  <h1 class="display-4">COVID-19, India</h1>
+	<span class="small text-muted">+ = Added today</span>
+</div>
+
+<div class="card border-0 my-2 bg-transparent">
+    <div class="card-body p-0">
+      <div class="d-flex flex-row align-content-stretch flex-wrap justify-content-between">
+			  <div class="stat p-2">
+          <h3>+{newConfirmed}</h3>
+          <p>Confirmed {totalConfirmed}</p>
+        </div>
+        <div class="stat p-2">
+          <h3>+{newDeaths}</h3>
+          <p>Deaths {totalDeaths}</p>
+        </div>
+        <div class="stat p-2">
+          <h3>+{newRecovered}</h3>
+          <p>Recovered {totalRecovered}</p>
+        </div>
+      </div>
+    </div>
+  </div>

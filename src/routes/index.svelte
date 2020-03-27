88487d98bdddcd60333959657337d0475dccf995
abhishek <akmnitt@gmail.com>
2020-03-27 23:31:04 +0530

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,41 +1,42 @@
 <script>
-	import { onMount } from "svelte";
-	const dt = new Date();
-	const start = `${dt.getFullYear()}-0${dt.getMonth() +
-	  1}-${dt.getDate()}T00:00:00Z`;
-	let data;
+  import { onMount } from "svelte";
+  const dt = new Date();
+  const start = `${dt.getFullYear()}-0${dt.getMonth() +
+    1}-${dt.getDate()}T00:00:00Z`;
+  let data;
 
-	let totalConfirmed = 0;
-	let newConfirmed = 0;
-	const confirmedurl = `https://api.covid19api.com/live/country/india/status/confirmed/date/${start}`;
-	onMount(async () => {
-	  const res = await fetch(confirmedurl);
-	  const json = await res.json();
-	  data = json.sort((a, b) => new Date(b.Date) - new Date(a.Date));
-	  newConfirmed = Number(data[0].Cases) - Number(data[data.length - 1].Cases);
-	  totalConfirmed = data[0].Cases;
-	});
-	let totalDeaths = 0;
-	let newDeaths = 0;
-	const deathsurl = `https://api.covid19api.com/live/country/india/status/deaths/date/${start}`;
-	onMount(async () => {
-	  const res = await fetch(deathsurl);
-	  const json = await res.json();
-	  data = json.sort((a, b) => new Date(b.Date) - new Date(a.Date));
-	  newDeaths = Number(data[0].Cases) - Number(data[data.length - 1].Cases);
-	  totalDeaths = data[0].Cases;
-	});
-	let totalRecovered = 0;
-	let newRecovered = 0;
-	const recoveredurl = `https://api.covid19api.com/live/country/india/status/recovered/date/${start}`;
-	onMount(async () => {
-	  const res = await fetch(recoveredurl);
-	  const json = await res.json();
-	  data = json.sort((a, b) => new Date(b.Date) - new Date(a.Date));
-	  newRecovered = Number(data[0].Cases) - Number(data[data.length - 1].Cases);
-	  totalRecovered = data[0].Cases;
-	});
+  let totalConfirmed = 0;
+  let newConfirmed = 0;
+  const confirmedurl = `https://api.covid19api.com/live/country/india/status/confirmed/date/${start}`;
+  onMount(async () => {
+    const res = await fetch(confirmedurl);
+    const json = await res.json();
+    data = json.sort((a, b) => new Date(b.Date) - new Date(a.Date));
+    newConfirmed = Number(data[0].Cases) - Number(data[data.length - 1].Cases);
+    totalConfirmed = data[0].Cases;
+  });
+  let totalDeaths = 0;
+  let newDeaths = 0;
+  const deathsurl = `https://api.covid19api.com/live/country/india/status/deaths/date/${start}`;
+  onMount(async () => {
+    const res = await fetch(deathsurl);
+    const json = await res.json();
+    data = json.sort((a, b) => new Date(b.Date) - new Date(a.Date));
+    newDeaths = Number(data[0].Cases) - Number(data[data.length - 1].Cases);
+    totalDeaths = data[0].Cases;
+  });
+  let totalRecovered = 0;
+  let newRecovered = 0;
+  const recoveredurl = `https://api.covid19api.com/live/country/india/status/recovered/date/${start}`;
+  onMount(async () => {
+    const res = await fetch(recoveredurl);
+    const json = await res.json();
+    data = json.sort((a, b) => new Date(b.Date) - new Date(a.Date));
+    newRecovered = Number(data[0].Cases) - Number(data[data.length - 1].Cases);
+    totalRecovered = data[0].Cases;
+  });
 </script>
+
 <style>
   /* Stat */
   .stat {
@@ -61,30 +62,68 @@
     padding-left: 0.5rem;
   }
 </style>
+
 <svelte:head>
-	<title>COVID-19 India</title>
+  <title>COVID-19 India</title>
 </svelte:head>
 
 <div class="jumbotron">
-  <h1 class="display-4">COVID-19, India</h1>
-	<span class="small text-muted">+ = Added today</span>
+  <h2 class="display-4">COVID-19, India</h2>
+  <span class="small text-muted">+ = Added today</span>
 </div>
 
 <div class="card border-0 my-2 bg-transparent">
-    <div class="card-body p-0">
-      <div class="d-flex flex-row align-content-stretch flex-wrap justify-content-between">
-			  <div class="stat p-2">
-          <h3>+{newConfirmed}</h3>
-          <p>Confirmed {totalConfirmed}</p>
-        </div>
-        <div class="stat p-2">
-          <h3>+{newDeaths}</h3>
-          <p>Deaths {totalDeaths}</p>
-        </div>
-        <div class="stat p-2">
-          <h3>+{newRecovered}</h3>
-          <p>Recovered {totalRecovered}</p>
-        </div>
+  <div class="card-body p-0">
+    <div
+      class="d-flex flex-row align-content-stretch flex-wrap
+      justify-content-between">
+      <div class="stat p-2">
+        <h3>+{newConfirmed}</h3>
+        <p>Confirmed {totalConfirmed}</p>
+      </div>
+      <div class="stat p-2">
+        <h3>+{newDeaths}</h3>
+        <p>Deaths {totalDeaths}</p>
+      </div>
+      <div class="stat p-2">
+        <h3>+{newRecovered}</h3>
+        <p>Recovered {totalRecovered}</p>
       </div>
     </div>
   </div>
+</div>
+<div class="py-5 bg-light text-muted">
+  <br />
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-8">
+        <div class="font-weight-bold text-uppercase text-lg text-dark mb-3">
+          COVID-19
+        </div>
+        <p>
+          Fun project to play with
+          <a href="https://covid19api.com/" target="_blank" rel="noopener">
+            api
+          </a>
+        </p>
+        <p>
+          Developed By
+          <a
+            href="https://xpressabhi.github.io/"
+            target="_blank"
+            rel="noopener">
+            Abhishek Maurya
+          </a>
+        </p>
+
+      </div>
+      <div class="col-md-4">
+        <img
+          src="https://res.cloudinary.com/xpsabhi/image/upload/v1547488584/undraw_contact_us_15o2.svg"
+          class="img-fluid"
+          alt="" />
+      </div>
+    </div>
+  </div>
+  <br />
+</div>
